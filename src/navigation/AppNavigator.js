fd652cc647a991a4cedac5b6b956cbda430a0e0c
KRISHNA KUMAR MANDAL <kkmandal2030@gmail.com>
2025-04-03 23:31:51 +0530

    DesiRasoi Recipe App Project

---
+++ src/navigation/AppNavigator.js
@@ -0,0 +1,19 @@
+import { NavigationContainer } from "@react-navigation/native";
+import React from "react";
+import {} from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import WelcomeScreen from "../screens/WelcomeScreen";
+
+const Stack = createNativeStackNavigator();
+
+const AppNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppNavigator;
---
+++ src/screens/WelcomeScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const WelcomeScreen = () => {
+  return (
+    <View>
+      <Text>Krishna</Text>
+    </View>
+  );
+};
+
+export default WelcomeScreen;
