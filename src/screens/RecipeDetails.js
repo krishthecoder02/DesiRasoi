5541682129933fa273c6270cab8b301f13380ad5
KRISHNA KUMAR MANDAL <kkmandal2030@gmail.com>
2025-04-08 23:11:47 +0530

    Update RecipeDetails.js

--- src/screens/RecipeDetails.js
+++ src/screens/RecipeDetails.js
@@ -1,27 +1,52 @@
 import React from "react";
-import { View, Text, StyleSheet, FlatList, ScrollView } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
 import { FontAwesome } from "@expo/vector-icons";
 
-const RecipeDetails = ({ route }) => {
+const RecipeDetails = ({ route, navigation }) => {
   const { item } = route.params;
 
   return (
     <View style={styles.container}>
       <View style={{ backgroundColor: "#D84040", paddingVertical: wp(6) }}>
-        <FontAwesome
-          name="arrow-left"
-          size={20}
-          color="#fff"
-          style={styles.fontAwesome}
-        />
-        <Text style={[styles.header, { top: wp(7), paddingHorizontal: wp(8) }]}>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <FontAwesome
+            name="arrow-left"
+            size={20}
+            color="#fff"
+            style={styles.fontAwesome}
+          />
+        </TouchableOpacity>
+        <Text
+          style={[styles.header, { top: wp(7), paddingHorizontal: wp(18) }]}
+        >
           🍛 {item.dish} बनाने की विधि:
         </Text>
       </View>
+      {/* <TouchableOpacity style={{ padding: wp(8), flexDirection: "row" , backgroundColor:""}}>
+        <FontAwesome
+          name="arrow-right"
+          size={20}
+          color="#D84040"
+          style={[styles.fontAwesome, {}]}
+        />
+        <Text>{item.dish} बनाने की विधि</Text>
+      </TouchableOpacity> */}
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>
+          ➡️ {item.dish} में क्या-क्या लगेगा?
+        </Text>
+      </TouchableOpacity>
 
       <FlatList
         data={item.vidhi}
@@ -68,6 +93,27 @@ const styles = StyleSheet.create({
   description: {
     fontSize: 16,
     color: "#4E342E",
+  },
+  button: {
+    backgroundColor: "#fdcb6e",
+    padding: 12,
+    borderRadius: 10,
+    alignItems: "center",
+    marginBottom: wp(2),
+    marginTop: wp(2),
+    marginHorizontal: wp(2),
+    elevation: 10,
+    borderWidth: 1,
+    borderColor: "#ffffff",
+  },
+  buttonText: {
+    color: "#2d3436",
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+  fontAwesome: {
+    top: wp(13),
+    left: wp(3),
   },
 });
 
