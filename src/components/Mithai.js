eb1e3e2b64f855bb7d4fab092014ac9040e3a92f
KRISHNA KUMAR MANDAL <kkmandal2030@gmail.com>
2025-04-09 23:05:49 +0530

    some update

--- src/components/Mithai.js
+++ src/components/Mithai.js
@@ -1,28 +1,108 @@
+// import React from "react";
+// import { View, Text, StyleSheet, FlatList, Image } from "react-native";
+// import {
+//   widthPercentageToDP as wp,
+//   heightPercentageToDP as hp,
+// } from "react-native-responsive-screen";
+// import { mithaiList } from "../constant";
+
+// const Mithai = () => {
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.text}>मिठाई</Text>
+//       <View>
+//         <FlatList
+//           data={mithaiList}
+//           horizontal
+//           keyExtractor={(item) => item.id}
+//           renderItem={({ item }) => (
+//             <View
+//               style={{
+//                 padding: wp(1),
+//                 // borderBottomWidth: 1,
+//                 height: 145,
+//                 width: 115,
+//               }}
+//             >
+//               <View
+//                 style={{
+//                   backgroundColor: "#ffff",
+//                   borderRadius: 10,
+//                   alignItems: "center",
+//                   elevation: 10,
+//                   padding: 3,
+//                 }}
+//               >
+//                 <Image
+//                   source={item.img}
+//                   style={{ height: 100, width: 100, resizeMode: "center" }}
+//                 />
+//                 <Text style={{ color: "#000", fontSize: 16, marginBottom: 4 }}>
+//                   {item.dish}
+//                 </Text>
+//               </View>
+//             </View>
+//           )}
+//         />
+//       </View>
+//     </View>
+//   );
+// };
+
+// export default Mithai;
+
+// const styles = StyleSheet.create({
+//   text: {
+//     color: "#000000",
+//     fontSize: wp(5),
+//     fontWeight: "bold",
+//     paddingHorizontal: wp(3),
+//   },
+//   container: {
+//     backgroundColor: "#f2f2f2",
+
+//     padding: wp(1 / 2),
+//   },
+// });
 import React from "react";
-import { View, Text, StyleSheet, FlatList, Image } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  Image,
+  Pressable,
+} from "react-native";
+
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
+
 import { mithaiList } from "../constant";
+import { useNavigation } from "@react-navigation/native";
 
 const Mithai = () => {
+  const navigation = useNavigation();
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>मिठाई</Text>
+      <Text style={styles.text}>नाश्ता</Text>
       <View>
         <FlatList
           data={mithaiList}
           horizontal
           keyExtractor={(item) => item.id}
           renderItem={({ item }) => (
-            <View
+            <Pressable
               style={{
                 padding: wp(1),
                 // borderBottomWidth: 1,
                 height: 145,
                 width: 115,
               }}
+              onPress={() =>
+                navigation.navigate("RecipeDetails", { item: item })
+              }
             >
               <View
                 style={{
@@ -37,11 +117,13 @@ const Mithai = () => {
                   source={item.img}
                   style={{ height: 100, width: 100, resizeMode: "center" }}
                 />
-                <Text style={{ color: "#000", fontSize: 16, marginBottom: 4 }}>
+                <Text
+                  style={{ color: "#6D4C41", fontSize: 16, marginBottom: 4 }}
+                >
                   {item.dish}
                 </Text>
               </View>
-            </View>
+            </Pressable>
           )}
         />
       </View>
@@ -49,11 +131,9 @@ const Mithai = () => {
   );
 };
 
-export default Mithai;
-
 const styles = StyleSheet.create({
   text: {
-    color: "#000000",
+    color: "#6D4C41",
     fontSize: wp(5),
     fontWeight: "bold",
     paddingHorizontal: wp(3),
@@ -64,3 +144,5 @@ const styles = StyleSheet.create({
     padding: wp(1 / 2),
   },
 });
+
+export default Mithai;
